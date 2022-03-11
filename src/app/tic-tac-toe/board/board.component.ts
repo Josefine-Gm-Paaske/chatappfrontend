3b24cbde383a8754d3b8d92146ca9c7b5f689ea8
Josefine-Gm-Paaske <74040649+Josefine-Gm-Paaske@users.noreply.github.com>
2022-03-11 12:15:16 +0100

    url removed

--- src/app/tic-tac-toe/board/board.component.ts
+++ src/app/tic-tac-toe/board/board.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-board',
-  templateUrl: './board.component.html',
+  template: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
